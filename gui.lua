26179cfffce06b892d423052c484fd8a568123f7
rarechive <huynek389@gmail.com>
2025-04-13 16:02:12 +0700

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,10 +1,27 @@
 -- gui.lua
-return function(Window)
-    -- Thêm tab Information
-    local Tab1 = Window:AddTab({ Title = "Information", Icon = "info" })
-    Tab1:AddParagraph("Chào mừng đến RareXploit", "Hub này được tạo bởi Rarechive.")
+
+return function(Hub)
+    -- Tạo tab Information
+    local InfoTab = Hub:CreateTab("Information", "rbxassetid://1234567890")
     
-    -- Thêm tab Settings
-    local Tab2 = Window:AddTab({ Title = "Settings", Icon = "settings" })
-    Tab2:AddToggle("Toggle1", { Text = "Tùy chọn mẫu", Default = false })
+    -- Thêm các thành phần vào tab Information
+    InfoTab:CreateLabel("Welcome to Dawid Hub!")
+    InfoTab:CreateParagraph("This is a hub created using Fluent library.")
+    InfoTab:CreateButton("Click me!", function()
+        print("Button clicked!")
+    end)
+    
+    -- Tạo tab Setting
+    local SettingTab = Hub:CreateTab("Setting", "rbxassetid://0987654321")
+    
+    -- Thêm các thành phần vào tab Setting
+    SettingTab:CreateToggle("Toggle something", false, function(state)
+        print("Toggle state:", state)
+    end)
+    SettingTab:CreateSlider("Slider", 0, 100, 50, function(value)
+        print("Slider value:", value)
+    end)
+    SettingTab:CreateDropdown("Dropdown", {"Option 1", "Option 2", "Option 3"}, "Option 1", function(option)
+        print("Selected option:", option)
+    end)
 end
