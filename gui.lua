b8aeef7b3aab1ecd60f3dafdb77b44cdfef046d7
rarechive <huynek389@gmail.com>
2025-04-13 19:19:38 +0700

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -2,7 +2,7 @@ local gui = {}
 
 function gui.CreateGUI(RareXploit)
     local Window = RareXploit:CreateWindow({
-        Title = "RareXploit 0.1",
+        Title = "RareXploit 0.2",
         SubTitle = "by Rarechive",
         TabWidth = 160,
         Size = UDim2.fromOffset(500, 400),
